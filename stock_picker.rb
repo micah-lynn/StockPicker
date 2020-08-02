abc6d49b8479a3bbdad1775d331c1ff37b1dfcc9
Micah Lynn <micah@sabado.com>
2020-08-02 17:35:47 -0400

    Program working with set input

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,21 @@
 #Create a variable called "stock prices" that is an array of potential stock prices
+p stock_prices = [24,13,6,4,8,3,6,9,2]
+
 #Create a method called stock picker which takes an array
-#Create a method called "buy low", which evaluates an array and returns the smallest number index
-#Create a method called "sell high", which evaluates an array and returns the highest number index
+def stock_picker(array)
+    sorted_stocks = array.sort.reverse
+    profit = 0
+    days_buy_sell = []
+    sorted_stocks.each do |price_sell|
+        array.each do |price_buy|
+            if price_buy < price_sell && array.index(price_sell) > array.index(price_buy) && price_sell - price_buy > profit
+                days_buy_sell = [array.index(price_buy), array.index(price_sell)]
+                profit = price_sell - price_buy
+            end
+        end
+    end
+    days_buy_sell
+end
 
 #Print outputs here
+p stock_picker(stock_prices)
