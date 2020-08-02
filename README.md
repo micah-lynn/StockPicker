1af3e12769473f35c2903509cb7338df2c5ec284
Micah Lynn <micah@sabado.com>
2020-08-02 14:27:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stock Picker
+
+Create a basic stock picker in ruby.
+
+*It should evaluate an array and determine the best time to buy and sell (Buy low, sell high)*
---
+++ stock_picker.rb
@@ -0,0 +1,6 @@
+#Create a variable called "stock prices" that is an array of potential stock prices
+#Create a method called stock picker which takes an array
+#Create a method called "buy low", which evaluates an array and returns the smallest number index
+#Create a method called "sell high", which evaluates an array and returns the highest number index
+
+#Print outputs here
